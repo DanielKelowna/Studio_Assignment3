9f00ed2002e86bdb5019175aec607cb5b28bf123
DanielKelowna <dankelowna@gmail.com>
2025-03-05 21:35:50 -0800

    Update

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [System.Serializable]
+    public class Vector2Event : UnityEvent<Vector2> { }
 
-    // Update is called once per frame
-    void Update()
+    // These events allow other scripts to subscribe to movement, jump, and reset actions.
+    public Vector2Event OnMove;
+    public UnityEvent OnJump;
+    public UnityEvent OnReset;
+
+    private void Update()
     {
-        
+        // Handle movement input using the horizontal and vertical axes.
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (moveInput != Vector2.zero)
+        {
+            OnMove?.Invoke(moveInput);
+        }
+
+        // When space is pressed, invoke jump.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnJump?.Invoke();
+        }
+
+        // For resetting the player, here we use the R key.
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            OnReset?.Invoke();
+        }
     }
 }
